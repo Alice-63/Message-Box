485fce9827a6bca762c9d3c21a5e6519d9f273c2
Alice-63 <alicetindag84@gmail.com>
2021-07-28 12:07:06 +0200

    add

--- script.js
+++ script.js
@@ -7,11 +7,17 @@ $(document).ready(function(){
             top:"20vh",
             width:"700px",
             height:"430px",
-            fontSize:"36px"
-        },2000)
+            fontSize:"36px",
+            boxShadow:"-1px 1px 14px 0px #000000;"
+        },2000,function(){
+            $("#icon").fadeIn(500)
+            $("#iconMinus").fadeIn(500)
+        })
     })
 
-
+    $("#icon").click(function(){
+        $("#message").slideUp(1000)
+    })
 })
 
    
